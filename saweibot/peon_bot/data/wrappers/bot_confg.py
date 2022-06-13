aedbdda12eb16ac46eb3e62e73d967c610847907
saweima12 <saweima12@gmail.com>
2022-06-13 18:18:43 +0800

    re organize routes

--- saweibot/peon_bot/data/wrappers/bot_confg.py
+++ saweibot/peon_bot/data/wrappers/bot_confg.py
@@ -1,13 +1,10 @@
-from aiogram import Bot
+from saweibot.core.entities import BotConfig
+from saweibot.core.wrapper import BaseModelWrapper
 from saweibot.storages.redis import RedisJsonObject
 
-from saweibot.core.wrapper import BaseModelWrapper
-from saweibot.core.entities import BotConfig
-
 from ..models import BotConfigModel
-from ..meta import SERVICE_CODE
 
-class BotConfigWrapper(BaseModelWrapper):
+class BotConfigWrapper(BaseModelWrapper[BotConfigModel, RedisJsonObject]):
 
     __model__ = BotConfigModel
 
@@ -15,30 +12,25 @@ class BotConfigWrapper(BaseModelWrapper):
         self.bot_id = bot_id
         super().__init__()
 
-    @property
-    def proxy(self) -> RedisJsonObject:
-        return self.factory(SERVICE_CODE).get_json_obj("config")
+    def _proxy(self) -> RedisJsonObject:
+        return self.factory(self.bot_id).get_json_obj("config")
 
-    async def from_proxy(self):
+    async def _from_proxy(self):
         result = await self.proxy.get()
         if result:
             return BotConfigModel(**result)
         return None
 
-    async def from_db(self):
+    async def _from_db(self):
         result = await BotConfig.get_or_none(bot_id=self.bot_id)
         if result:
             return BotConfigModel(**result.conf_json)
         return None
 
-    async def save(self, data: BotConfigModel=None):
-        _data = data if data else (await self.get_model())
-        await self.proxy.set(_data.json())
+    async def _save_proxy(self, data: BotConfigModel=None):
+        await self.proxy.set(data.dict())
     
-    async def save_db(self, data: BotConfigModel=None):
-        _data = data if data else (await self.get_model())
-        print(_data.dict(), self.bot_id)
-
+    async def _save_db(self, data: BotConfigModel=None):
         await BotConfig.update_or_create({
-            'conf_json': _data.dict()
+            'conf_json': data.dict()
         }, bot_id=self.bot_id)
