5226a34b4de67d604ddd49249072db9eac7abd35
saweima12 <saweima12@gmail.com>
2022-06-20 00:43:18 +0800

    change process sequeence

--- saweibot/services/scheduler/__init__.py
+++ saweibot/services/scheduler/__init__.py
@@ -6,12 +6,11 @@ def register(app: Sanic):
 
     _scheduler = scheduler.setup(app)
 
-
-    @app.before_server_start
+    @app.after_server_start
     async def startup(app: Sanic, _):
         await _scheduler.run_scheduler(app)
 
 
     @app.before_server_stop
     async def dispose(app: Sanic, _):
-        await scheduler.dispose(app)
+        await _scheduler.stop_scheduler()
--- saweibot/services/scheduler/scheduler.py
+++ saweibot/services/scheduler/scheduler.py
@@ -4,7 +4,6 @@ from .struct import AppScheduler
 
 SERVICE_CODE = "app_scheduler"
 
-
 def get() -> AppScheduler:
     app = Sanic.get_app()
     return getattr(app.ctx, SERVICE_CODE)
@@ -14,8 +13,3 @@ def setup(app: Sanic) -> AppScheduler:
     scheduler = AppScheduler(app)
     setattr(app.ctx, SERVICE_CODE, scheduler)
     return scheduler
-
-async def dispose(app: Sanic):
-    if hasattr(app.ctx, SERVICE_CODE):
-        _scheduler: AppScheduler = getattr(app.ctx, SERVICE_CODE)
-        await _scheduler.stop_scheduler()
