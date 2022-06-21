8d9ca23020269745a5368e92b02dff9ed4ddeeba
saweima12 <saweima12@gmail.com>
2022-06-21 23:35:30 +0800

    remove rmall & addpoint command

--- saweibot/bussiness/command/__init__.py
+++ saweibot/bussiness/command/__init__.py
@@ -1,13 +1,9 @@
 from saweibot.common import CommandMap
 
-from .rmall import remove_msg_by_user
-from .addpoint import add_record_point
 from .setpoint import set_reocrd_point
 from .point import get_point
 
 map = CommandMap(prefix='$')
 
-map.register_command_handler("rmall", remove_msg_by_user)
-map.register_command_handler("addpoint", add_record_point)
 map.register_command_handler("setpoint", set_reocrd_point)
 map.register_command_handler("point", get_point)
