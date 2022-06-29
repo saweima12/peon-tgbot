8b42c8cc69d96debad2a7ad046426f87c2eaec33
saweima12 <saweima12@gmail.com>
2022-06-29 13:15:31 +0800

    add multiple worker support and multiple member query

--- saweibot/bussiness/command/query.py
+++ saweibot/bussiness/command/query.py
@@ -4,7 +4,7 @@ from sanic.log import logger
 
 
 from saweibot.data.wrappers import BehaviorRecordWrapper
-from saweibot.text import QUERY_SUCCESS_TIPS, QUERY_FAILED_TIPS
+from saweibot.text import QUERY_SUCCESS_TIPS, QUERY_FAILED_TIPS, MEMBER_LINK
 from ..helper import MessageHelepr
 
 async def query_user(*params, helper: MessageHelepr):
@@ -13,20 +13,33 @@ async def query_user(*params, helper: MessageHelepr):
     if not (await helper.is_group_admin()) and not (await helper.is_whitelist_user()):
         return
 
-    params_count = len(params)
-    if params_count == 1:
-        target_id = params[0]
-        _tips = None
-        if target_id is None:
-            return 
+    if len(params) < 1:
+        return
 
-        # get chat member
-        try :
-            member = await helper.bot.get_chat_member(helper.chat_id, target_id)
-            # send user's metion.
-            _tips = await helper.bot.send_message(helper.chat_id, QUERY_SUCCESS_TIPS.format(full_name=member.user.full_name,
-                                                                user_id=member.user.id), parse_mode='Markdown')
-            logger.info(f"User {helper.user.full_name} query member_id {target_id}")
+
+    query_str= "".join(params)
+    members_str = []
+
+    # get chat's member data.
+    for member_id in params:
+        try:
+            member = await helper.bot.get_chat_member(helper.chat_id, member_id)
+            members_str.append(MEMBER_LINK.format(full_name=member.user.full_name, user_id=member.user.id))
         except Exception as _e:
-            _tips = await helper.bot.send_message(helper.chat_id, QUERY_FAILED_TIPS.format(user_id=target_id), parse_mode='Markdown')
-            logger.error(_e)
+            logger.info(f"User not found {member_id}")
+
+    if not members_str:
+        # query is empty
+        _tips = await helper.bot.send_message(helper.chat_id, QUERY_FAILED_TIPS, parse_mode='Markdown')
+        logger.info("Query failed, object not found.")
+        return
+
+    try :
+        _content = "\n".join(members_str)
+        # send user's metion.
+        _msg = QUERY_SUCCESS_TIPS.format(content=_content, count=len(members_str))
+        
+        _tips = await helper.bot.send_message(helper.chat_id, _msg, parse_mode='Markdown')
+        logger.info(f"User {helper.user.full_name} query member_id {query_str}")
+    except Exception as _e:
+        logger.error(_e)
--- saweibot/text.py
+++ saweibot/text.py
@@ -6,5 +6,7 @@ NEED_REPLY_MESSAGE = "必須 Reply 一個訊息"
 FIRST_URL_TIPS = "[@{full_name}](tg://user?id={user_id}) 初次發言不可包含連結, 請再試一次。"
 MEDIA_MESSAGE_TIPS = "[@{full_name}](tg://user?id={user_id}) 發言點數低於 {min_point}。暫時禁止純文字以外的訊息。如有任何問題請聯繫管理員。"
 
-QUERY_SUCCESS_TIPS = "查詢成功 [@{full_name}](tg://user?id={user_id})"
-QUERY_FAILED_TIPS="查詢失敗，找不到對象 id {user_id}"
+MEMBER_LINK = "[@{full_name}](tg://user?id={user_id})"
+
+QUERY_SUCCESS_TIPS = "查詢成功，共計 {count} 筆。\n {content}"
+QUERY_FAILED_TIPS="查詢失敗，找不到對象。"
