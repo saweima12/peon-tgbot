bc44a25c597dff4491fec7e5f9cb4cf639b87c44
saweima12 <saweima12@gmail.com>
2022-07-01 22:59:42 +0800

    add everyday tips

--- saweibot/views/bot.py
+++ saweibot/views/bot.py
@@ -1,8 +1,10 @@
+import datetime as dt
 from sanic import HTTPResponse, Request, Blueprint, response, text
 from sanic.log import logger
 from aiogram.types import Update
 
-
+from saweibot.text import DELETED_COUNT_TIPS, DELETED_PAGE
+from saweibot.data.entities import ChatDeletedMessage, PeonChatConfig
 from saweibot.services import bot
 
 bp = Blueprint("peon_bot", url_prefix="/peon")
@@ -50,3 +52,28 @@ async def peon(request: Request, token: str) -> HTTPResponse:
 
     return response.empty(status=400)
 
+
+@bp.get("/<token:str>/send_deleted")
+async def send_deleted_tips(request: Request, token: str):
+    
+    # get deleted message
+    _bot = bot.get_bot()
+    if _bot._token != token:
+        return response.empty(status=404)
+
+    chats = await PeonChatConfig.filter(status="ok")
+
+    now = dt.datetime.combine(dt.datetime.utcnow(), dt.time(15, 0))
+    start = now - dt.timedelta(days=1)
+
+    for chat in chats:
+
+        chat_id = chat.chat_id
+
+        msg_count = await ChatDeletedMessage.filter(chat_id=chat_id, record_date__gte=start).count()
+        
+        _page_url = DELETED_PAGE.format(chat_id=chat_id)
+        _text = DELETED_COUNT_TIPS.format(count=str(msg_count), url=_page_url)
+        await _bot.send_message(chat_id, _text, parse_mode='Markdown')
+    
+    return response.empty(200)
