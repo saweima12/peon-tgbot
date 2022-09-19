daf6fc220782252b66f2db61ca8e083510134d98
saweima12 <saweima12@gmail.com>
2022-09-19 17:51:00 +0800

    add sub point on query

--- saweibot/bussiness/command/point.py
+++ saweibot/bussiness/command/point.py
@@ -11,11 +11,14 @@ async def get_point(*params, helper: MessageHelepr):
 
     wrapper = helper.behavior_wrapper()
     _model = await wrapper.get(helper.user_id)
+
     temp = await helper.msg.reply(f"Point: {_model.msg_count}")
     logger.info(f"User [{helper.user.full_name}]({helper.user_id}) query point: {_model.msg_count}")
     await asyncio.sleep(3)
-
+    # sub msg count
+    _model.msg_count -= 5
     await asyncio.gather(
         temp.delete(),
-        helper.msg.delete()
+        helper.msg.delete(),
+        wrapper.save_db(helper.user_id, _model)
     )
